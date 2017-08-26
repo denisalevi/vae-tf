d36349c7c306eccd7aa4950760a523a24d3a44a9
denisalevi <mail@denisalevi.de>
2017-08-26 20:50:29 +0100

    Fix plot_accuracies.py bug

--- vae_tf/scripts/plot_accuracies.py
+++ vae_tf/scripts/plot_accuracies.py
@@ -4,9 +4,9 @@ import argparse
 parser = argparse.ArgumentParser(description='Plot accuracies from a logfile created by run_clustering.py')
 parser.add_argument('accuracy_file', nargs=1, type=str,
                     help='An accuracy file created by run_clustering.py')
-parser.add_argument('--sort_by', nargs=1, default=None, type=str,
+parser.add_argument('--sort_by', nargs=1, default=[None], type=str,
                     help='What to sort the accuracies by (beta, arch, some accuracy)')
-parser.add_argument('--index', nargs=1, default=None, type=str,
+parser.add_argument('--index', nargs=1, default=[None], type=str,
                     help='Plot accuracies for a ceratin index in data matrix (one arch or beta)')
 args = parser.parse_args()
 
